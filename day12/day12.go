ba7b1284d00c044726bffce3f071d608b15ce319
scottdharris <scottdharris11@yahoo.com>
2022-12-13 05:21:39 -0500

    day 12 part 1

--- day12/day12.go
+++ day12/day12.go
@@ -1,7 +1,9 @@
 package day12
 
 import (
+	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"advent-of-code-2022/utils"
@@ -17,7 +19,8 @@ func (Puzzle) Solve() {
 
 func solvePart1(grid []string) int {
 	start := time.Now().UnixMilli()
-	ans := len(grid)
+	rp := NewRoutePlanner(grid)
+	ans := rp.Route()
 	end := time.Now().UnixMilli()
 	log.Printf("Day 12, Part 1 (%dms): Shortest Route = %d", end-start, ans)
 	return ans
@@ -29,4 +32,115 @@ func solvePart2(grid []string) int {
 	end := time.Now().UnixMilli()
 	log.Printf("Day 12, Part 2 (%dms): Shortest Route = %d", end-start, ans)
 	return ans
+}
+
+func NewRoutePlanner(grid []string) *RoutePlanner {
+	r := &RoutePlanner{}
+	for y, row := range grid {
+		nRow := row
+		if idx := strings.IndexRune(row, 'S'); idx >= 0 {
+			r.startLocation = Position{idx, y}
+			nRow = strings.Join([]string{nRow[:idx], "a", nRow[idx+1:]}, "")
+		}
+		if idx := strings.IndexRune(row, 'E'); idx >= 0 {
+			r.goalLocation = Position{idx, y}
+			nRow = strings.Join([]string{nRow[:idx], "z", nRow[idx+1:]}, "")
+		}
+		r.grid = append(r.grid, nRow)
+	}
+	return r
+}
+
+type RoutePlanner struct {
+	grid          []string
+	startLocation Position
+	goalLocation  Position
+}
+
+func (r *RoutePlanner) Route() int {
+	search := utils.Search{Searcher: r}
+	solution := search.Best(utils.SearchMove{
+		Cost: 0,
+		State: RouteState{
+			currentX: r.startLocation.x,
+			currentY: r.startLocation.y,
+		},
+	})
+	return solution.Cost
+}
+
+func (r *RoutePlanner) Goal(state interface{}) bool {
+	var routeState = state.(RouteState)
+	return routeState.currentX == r.goalLocation.x && routeState.currentY == r.goalLocation.y
+}
+
+func (r *RoutePlanner) DistanceFromGoal(state interface{}) int {
+	var routeState = state.(RouteState)
+	xDistance := routeState.currentX - r.goalLocation.x
+	if xDistance < 0 {
+		xDistance *= -1
+	}
+	yDistance := routeState.currentY - r.goalLocation.y
+	if yDistance < 0 {
+		yDistance *= -1
+	}
+	return xDistance + yDistance
+}
+
+func (r *RoutePlanner) PossibleNextMoves(state interface{}) []utils.SearchMove {
+	var routeState = state.(RouteState)
+	current := Position{x: routeState.currentX, y: routeState.currentY}
+
+	e := rune(r.grid[current.y][current.x])
+	up := Position{current.x, current.y - 1}
+	down := Position{current.x, current.y + 1}
+	left := Position{current.x - 1, current.y}
+	right := Position{current.x + 1, current.y}
+	var moves []utils.SearchMove
+	for _, pos := range []Position{up, down, left, right} {
+		if !r.available(e, pos) {
+			continue
+		}
+		move := utils.SearchMove{
+			Cost: 1,
+			State: RouteState{
+				currentX: pos.x,
+				currentY: pos.y,
+			},
+		}
+		moves = append(moves, move)
+	}
+	return moves
+}
+
+func (r *RoutePlanner) available(currElevation rune, pos Position) bool {
+	ok, e := r.elevation(pos)
+	if !ok {
+		return false
+	}
+	return int(e)-int(currElevation) <= 1
+}
+
+func (r *RoutePlanner) elevation(pos Position) (bool, rune) {
+	if pos.y < 0 || pos.y >= len(r.grid) {
+		return false, ' '
+	}
+	if pos.x < 0 || pos.x >= len(r.grid[0]) {
+		return false, ' '
+	}
+	return true, rune(r.grid[pos.y][pos.x])
+}
+
+type RouteState struct {
+	currentX int
+	currentY int
+}
+
+type Position struct {
+	x int
+	y int
+}
+
+func (p *Position) encode() string {
+	return fmt.Sprintf(`"%d::%d"`, p.x, p.y)
 }
--- day12/day12_test.go
+++ day12/day12_test.go
@@ -1,6 +1,7 @@
 package day12
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,11 +18,143 @@ var testGrid = []string{
 }
 
 func TestSolvePart1(t *testing.T) {
-	assert.Equal(t, 0, solvePart1(testGrid))
-	assert.Equal(t, 0, solvePart1(utils.ReadLines("day12", "day-12-input.txt")))
+	assert.Equal(t, 31, solvePart1(testGrid))
+	assert.Equal(t, 447, solvePart1(utils.ReadLines("day12", "day-12-input.txt")))
 }
 
 func TestSolvePart2(t *testing.T) {
-	assert.Equal(t, 0, solvePart2(testGrid))
-	assert.Equal(t, 0, solvePart2(utils.ReadLines("day12", "day-12-input.txt")))
+	// assert.Equal(t, 0, solvePart2(testGrid))
+	// assert.Equal(t, 0, solvePart2(utils.ReadLines("day12", "day-12-input.txt")))
+}
+
+func TestNewRoutePlanner(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	assert.Equal(t, []string{
+		"aabqponm",
+		"abcryxxl",
+		"accszzxk",
+		"acctuvwj",
+		"abdefghi",
+	}, rp.grid)
+	assert.Equal(t, Position{0, 0}, rp.startLocation)
+	assert.Equal(t, Position{5, 2}, rp.goalLocation)
+}
+
+func TestRoute(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	assert.Equal(t, 31, rp.Route())
+}
+
+func TestGoal(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	tests := []struct {
+		state    RouteState
+		expected bool
+	}{
+		{RouteState{currentX: 0, currentY: 0}, false},
+		{RouteState{currentX: 1, currentY: 1}, false},
+		{RouteState{currentX: 5, currentY: 2}, true},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.state), func(t *testing.T) {
+			assert.Equal(t, tt.expected, rp.Goal(tt.state))
+		})
+	}
+}
+
+func TestDistanceFromGoal(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	tests := []struct {
+		state    RouteState
+		expected int
+	}{
+		{RouteState{currentX: 0, currentY: 0}, 7},
+		{RouteState{currentX: 1, currentY: 1}, 5},
+		{RouteState{currentX: 5, currentY: 2}, 0},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.state), func(t *testing.T) {
+			assert.Equal(t, tt.expected, rp.DistanceFromGoal(tt.state))
+		})
+	}
+}
+
+func TestAvailablePositions(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	tests := []struct {
+		state    RouteState
+		expected []utils.SearchMove
+	}{
+		{RouteState{currentX: 1, currentY: 1}, []utils.SearchMove{
+			{Cost: 1, State: RouteState{currentX: 1, currentY: 0}},
+			{Cost: 1, State: RouteState{currentX: 1, currentY: 2}},
+			{Cost: 1, State: RouteState{currentX: 0, currentY: 1}},
+			{Cost: 1, State: RouteState{currentX: 2, currentY: 1}},
+		}},
+		{RouteState{currentX: 3, currentY: 2}, []utils.SearchMove{
+			{Cost: 1, State: RouteState{currentX: 3, currentY: 1}},
+			{Cost: 1, State: RouteState{currentX: 3, currentY: 3}},
+			{Cost: 1, State: RouteState{currentX: 2, currentY: 2}},
+		}},
+		{RouteState{currentX: 0, currentY: 2}, []utils.SearchMove{
+			{Cost: 1, State: RouteState{currentX: 0, currentY: 1}},
+			{Cost: 1, State: RouteState{currentX: 0, currentY: 3}},
+		}},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.state), func(t *testing.T) {
+			assert.Equal(t, tt.expected, rp.PossibleNextMoves(tt.state))
+		})
+	}
+}
+
+func TestAvailable(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	tests := []struct {
+		elevation rune
+		pos       Position
+		expected  bool
+	}{
+		{'a', Position{1, 1}, true},
+		{'c', Position{1, 1}, true},
+		{'a', Position{2, 1}, false},
+		{'b', Position{2, 1}, true},
+		{'c', Position{2, 1}, true},
+		{'d', Position{2, 1}, true},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			assert.Equal(t, tt.expected, rp.available(tt.elevation, tt.pos))
+		})
+	}
+}
+
+func TestElevation(t *testing.T) {
+	rp := NewRoutePlanner(testGrid)
+	tests := []struct {
+		pos      Position
+		valid    bool
+		expected rune
+	}{
+		{Position{-1, 0}, false, ' '},
+		{Position{0, -1}, false, ' '},
+		{Position{8, 0}, false, ' '},
+		{Position{0, 5}, false, ' '},
+		{Position{0, 0}, true, 'a'},
+		{Position{1, 1}, true, 'b'},
+		{Position{7, 1}, true, 'l'},
+		{Position{7, 4}, true, 'i'},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("%v", tt.pos), func(t *testing.T) {
+			v, e := rp.elevation(tt.pos)
+			assert.Equal(t, tt.valid, v)
+			assert.Equal(t, tt.expected, e)
+		})
+	}
 }
