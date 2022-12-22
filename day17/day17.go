469bb2e17cf0aea6e8df16f9aae0b2ac0cf4f10d
scottdharris <scottdharris11@yahoo.com>
2022-12-22 10:19:48 -0600

    not working yet

---
+++ day17/day17.go
@@ -0,0 +1,227 @@
+package day17
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"advent-of-code-2022/utils"
+)
+
+type Puzzle struct{}
+
+func (Puzzle) Solve() {
+	input := utils.ReadLines("day17", "day-17-input.txt")
+	solvePart1(input[0])
+	solvePart2(input[0])
+}
+
+func solvePart1(wind string) int {
+	start := time.Now().UnixMilli()
+	cave := NewCave(wind, RockPatterns)
+	var ans int
+	for i := 0; i < 2022; i++ {
+		ans = cave.dropRock()
+	}
+	end := time.Now().UnixMilli()
+	log.Printf("Day 17, Part 1 (%dms): Rock Height = %d", end-start, ans)
+	return ans
+}
+
+func solvePart2(wind string) int {
+	start := time.Now().UnixMilli()
+	ans := len(wind)
+	end := time.Now().UnixMilli()
+	log.Printf("Day 17, Part 2 (%dms): Rock Height = %d", end-start, ans)
+	return ans
+}
+
+type WindDirection rune
+
+var Left WindDirection = '<'
+var Right WindDirection = '>'
+
+var RockPatterns = []Rock{
+	{height: 1, width: 4, pattern: []string{
+		"####",
+	}},
+	{height: 3, width: 3, pattern: []string{
+		" # ",
+		"###",
+		" # ",
+	}},
+	{height: 3, width: 3, pattern: []string{
+		"  #",
+		"  #",
+		"###",
+	}},
+	{height: 4, width: 1, pattern: []string{
+		"#",
+		"#",
+		"#",
+		"#",
+	}},
+	{height: 2, width: 2, pattern: []string{
+		"##",
+		"##",
+	}},
+}
+
+type Rock struct {
+	height  int
+	width   int
+	pattern []string
+}
+
+func (r Rock) CanShiftLeft(xOffset int, rows []string) bool {
+	if xOffset == 0 {
+		return false
+	}
+	for i := 0; i < r.height; i++ {
+		if r.pattern[i][0] == '#' && rows[i][xOffset-1] == '#' {
+			return false
+		}
+	}
+	return true
+}
+
+func (r Rock) CanShiftRight(xOffset int, rows []string) bool {
+	if xOffset+r.width-1 >= len(rows[0])-1 {
+		return false
+	}
+	for i := 0; i < r.height; i++ {
+		if r.pattern[i][r.width-1] == '#' && rows[i][xOffset+r.width] == '#' {
+			return false
+		}
+	}
+	return true
+}
+
+func (r Rock) CanShiftDown(xOffset int, rows []string) bool {
+	for h := r.height - 1; h >= 0; h-- {
+		below := rows[h+1]
+		bottom := r.pattern[h]
+		for i := 0; i < r.width; i++ {
+			if bottom[i] == '#' && below[i+xOffset] == '#' {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func NewCave(wind string, rocks []Rock) *Cave {
+	c := &Cave{width: 7, wind: wind, rocks: rocks}
+	c.rows = append(c.rows, "#######")
+	return c
+}
+
+type Cave struct {
+	width   int
+	wind    string
+	wOffset int
+	rocks   []Rock
+	rOffset int
+	rows    []string
+}
+
+func (c *Cave) windDirection() WindDirection {
+	w := c.wind[c.wOffset]
+	c.wOffset++
+	if c.wOffset >= len(c.wind) {
+		c.wOffset = 0
+	}
+	return WindDirection(w)
+}
+
+func (c *Cave) rock() Rock {
+	o := c.rOffset
+	c.rOffset++
+	if c.rOffset >= len(c.rocks) {
+		c.rOffset = 0
+	}
+	return c.rocks[o]
+}
+
+func (c *Cave) dropRock() int {
+	rock := c.rock()
+	xOffset := 2
+	yOffset := c.topRock() + 4
+
+	for {
+		impactRows := c.impactRows(yOffset, rock.height)
+
+		switch c.windDirection() {
+		case Left:
+			if rock.CanShiftLeft(xOffset, impactRows) {
+				xOffset--
+			}
+		case Right:
+			if rock.CanShiftRight(xOffset, impactRows) {
+				xOffset++
+			}
+		}
+
+		if !rock.CanShiftDown(xOffset, impactRows) {
+			c.addRock(xOffset, yOffset, rock)
+			break
+		}
+		yOffset--
+	}
+
+	return c.topRock()
+}
+
+func (c *Cave) impactRows(yOffset int, rockHeight int) []string {
+	var rows []string
+	caveHeight := len(c.rows)
+	for i := rockHeight; i >= 0; i-- {
+		caveRow := yOffset + i - 1
+		if caveRow >= caveHeight {
+			rows = append(rows, "       ")
+			continue
+		}
+		rows = append(rows, c.rows[caveRow])
+	}
+	return rows
+}
+
+func (c *Cave) addRock(xOffset int, yOffset int, rock Rock) {
+	caveHeight := len(c.rows)
+	for i := 0; i < rock.height; i++ {
+		caveRow := yOffset + i
+		var row []rune
+		switch {
+		case caveRow == caveHeight:
+			row = []rune("       ")
+			c.rows = append(c.rows, string(row))
+			caveHeight++
+		default:
+			row = []rune(c.rows[caveRow])
+		}
+		for j := 0; j < rock.width; j++ {
+			if rock.pattern[rock.height-i-1][j] == ' ' {
+				continue
+			}
+			row[j+xOffset] = '#'
+		}
+		c.rows[caveRow] = string(row)
+	}
+}
+
+func (c *Cave) topRock() int {
+	return len(c.rows) - 1
+}
+
+func (c *Cave) print() {
+	for i := len(c.rows) - 1; i >= 0; i-- {
+		row := strings.ReplaceAll(c.rows[i], " ", ".")
+		b := "|"
+		if i == 0 {
+			b = "+"
+			row = strings.ReplaceAll(row, "#", "-")
+		}
+		fmt.Printf("%s%s%s\n", b, row, b)
+	}
+}
---
+++ day17/day17_test.go
@@ -0,0 +1,475 @@
+package day17
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var testInput = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+
+func TestSolvePart1(t *testing.T) {
+	assert.Equal(t, 3068, solvePart1(testInput))
+	// assert.Equal(t, 0, solvePart1(utils.ReadLines("day17", "day-17-input.txt")[0]))
+}
+
+func TestRock_CanShiftLeft(t *testing.T) {
+	rock := Rock{height: 3, width: 3, pattern: []string{
+		" # ",
+		"###",
+		" # ",
+	}}
+	rows := []string{
+		"##     ",
+		"#     ",
+		"##     ",
+		"#######",
+	}
+
+	tests := []struct {
+		rows    []string
+		xOffset int
+		left    bool
+	}{
+		{rows, 0, false},
+		{rows, 1, false},
+		{rows, 2, true},
+		{[]string{
+			"#      ",
+			"##     ",
+			"#      ",
+			"#######",
+		}, 2, false},
+		{rows, 3, true},
+		{rows, 4, true},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			assert.Equal(t, tt.left, rock.CanShiftLeft(tt.xOffset, tt.rows))
+		})
+	}
+}
+
+func TestRock_CanShiftRight(t *testing.T) {
+	rock := Rock{height: 3, width: 3, pattern: []string{
+		" # ",
+		"###",
+		" # ",
+	}}
+	rows := []string{
+		"     ##",
+		"      #",
+		"     ##",
+		"#######",
+	}
+
+	tests := []struct {
+		rows    []string
+		xOffset int
+		right   bool
+	}{
+		{rows, 0, true},
+		{rows, 1, true},
+		{rows, 2, true},
+		{[]string{
+			"      #",
+			"     ##",
+			"      #",
+			"#######",
+		}, 2, false},
+		{rows, 3, false},
+		{rows, 4, false},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			assert.Equal(t, tt.right, rock.CanShiftRight(tt.xOffset, tt.rows))
+		})
+	}
+}
+
+func TestRock_CanShiftDown(t *testing.T) {
+	rock := Rock{height: 3, width: 3, pattern: []string{
+		" # ",
+		"###",
+		" # ",
+	}}
+	rows := []string{
+		"       ",
+		"       ",
+		"       ",
+		"     ##",
+	}
+
+	tests := []struct {
+		rows    []string
+		xOffset int
+		down    bool
+	}{
+		{rows, 0, true},
+		{rows, 1, true},
+		{rows, 2, true},
+		{rows, 3, true},
+		{[]string{
+			"       ",
+			"       ",
+			"     # ",
+			"     ##",
+		}, 3, false},
+		{rows, 4, false},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			assert.Equal(t, tt.down, rock.CanShiftDown(tt.xOffset, tt.rows))
+		})
+	}
+}
+
+func TestCave_WindDirection(t *testing.T) {
+	cave := NewCave("<<>><", RockPatterns)
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Right, cave.windDirection())
+	assert.Equal(t, Left, cave.windDirection())
+}
+
+func TestCave_RockPattern(t *testing.T) {
+	cave := NewCave("", RockPatterns)
+	assert.Equal(t, RockPatterns[0], cave.rock())
+	assert.Equal(t, RockPatterns[1], cave.rock())
+	assert.Equal(t, RockPatterns[2], cave.rock())
+	assert.Equal(t, RockPatterns[3], cave.rock())
+	assert.Equal(t, RockPatterns[4], cave.rock())
+
+	assert.Equal(t, RockPatterns[0], cave.rock())
+	assert.Equal(t, RockPatterns[1], cave.rock())
+	assert.Equal(t, RockPatterns[2], cave.rock())
+	assert.Equal(t, RockPatterns[3], cave.rock())
+	assert.Equal(t, RockPatterns[4], cave.rock())
+
+	assert.Equal(t, RockPatterns[0], cave.rock())
+	assert.Equal(t, RockPatterns[1], cave.rock())
+	assert.Equal(t, RockPatterns[2], cave.rock())
+	assert.Equal(t, RockPatterns[3], cave.rock())
+	assert.Equal(t, RockPatterns[4], cave.rock())
+}
+
+func TestCave_DropRock(t *testing.T) {
+	cave := NewCave(testInput, RockPatterns)
+
+	height := cave.dropRock()
+	assert.Equal(t, 1, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 4, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"   #   ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 6, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"####   ",
+		"  #    ",
+		"  #    ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 7, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 9, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 10, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+		" ####  ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 13, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+		" ####  ",
+		"  #    ",
+		" ###   ",
+		"  #    ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 15, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+		" ####  ",
+		"  #    ",
+		" ###   ",
+		"  #### ",
+		"     # ",
+		"     # ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 17, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+		" ####  ",
+		"  #    ",
+		" ###   ",
+		"  #### ",
+		"    ## ",
+		"    ## ",
+		"    #  ",
+		"    #  ",
+	}, cave.rows)
+
+	height = cave.dropRock()
+	assert.Equal(t, 17, height)
+	assert.Equal(t, []string{
+		"#######",
+		"  #### ",
+		"   #   ",
+		"  ###  ",
+		"#####  ",
+		"  # #  ",
+		"  # #  ",
+		"    #  ",
+		"    ## ",
+		"    ## ",
+		" ####  ",
+		"  #    ",
+		" ###   ",
+		"###### ",
+		"##  ## ",
+		"    ## ",
+		"    #  ",
+		"    #  ",
+	}, cave.rows)
+}
+
+func TestCave_ImpactRows(t *testing.T) {
+	cave := NewCave("", RockPatterns)
+	cave.rows = append(cave.rows, " ####  ")
+	cave.rows = append(cave.rows, "   ####")
+	cave.rows = append(cave.rows, "####   ")
+
+	tests := []struct {
+		yOffset  int
+		height   int
+		expected []string
+	}{
+		{8, 4, []string{
+			"       ",
+			"       ",
+			"       ",
+			"       ",
+			"       ",
+		}},
+		{4, 4, []string{
+			"       ",
+			"       ",
+			"       ",
+			"       ",
+			"####   ",
+		}},
+		{4, 1, []string{
+			"       ",
+			"####   ",
+		}},
+		{3, 4, []string{
+			"       ",
+			"       ",
+			"       ",
+			"####   ",
+			"   ####",
+		}},
+		{2, 4, []string{
+			"       ",
+			"       ",
+			"####   ",
+			"   ####",
+			" ####  ",
+		}},
+		{1, 4, []string{
+			"       ",
+			"####   ",
+			"   ####",
+			" ####  ",
+			"#######",
+		}},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			assert.Equal(t, tt.expected, cave.impactRows(tt.yOffset, tt.height))
+		})
+	}
+}
+
+func TestCave_AddRock(t *testing.T) {
+	tests := []struct {
+		before  []string
+		yOffset int
+		xOffset int
+		rock    Rock
+		after   []string
+		size    int
+	}{
+		{[]string{
+			"#######",
+		}, 1, 2, Rock{height: 2, width: 2, pattern: []string{
+			"##",
+			"##",
+		}}, []string{
+			"#######",
+			"  ##   ",
+			"  ##   ",
+		}, 2},
+		{[]string{
+			"      #",
+			"      #",
+			"      #",
+			"      #",
+			"#######",
+		}, 1, 2, Rock{height: 2, width: 2, pattern: []string{
+			"##",
+			"##",
+		}}, []string{
+			"#######",
+			"  ##  #",
+			"  ##  #",
+			"      #",
+			"      #",
+		}, 4},
+		{[]string{
+			"      #",
+			"      #",
+			"      #",
+			"      #",
+			"#######",
+		}, 1, 2, Rock{height: 3, width: 3, pattern: []string{
+			"  #",
+			"  #",
+			"###",
+		}}, []string{
+			"#######",
+			"  ### #",
+			"    # #",
+			"    # #",
+			"      #",
+		}, 4},
+		{[]string{
+			"     ##",
+			"      #",
+			"     ##",
+			"#######",
+		}, 1, 3, Rock{height: 3, width: 3, pattern: []string{
+			" # ",
+			"###",
+			" # ",
+		}}, []string{
+			"#######",
+			"    ###",
+			"   ####",
+			"    ###",
+		}, 3},
+	}
+	for i, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("Test %d", i), func(t *testing.T) {
+			cave := Cave{width: 7}
+			for i := len(tt.before) - 1; i >= 0; i-- {
+				cave.rows = append(cave.rows, tt.before[i])
+			}
+			cave.addRock(tt.xOffset, tt.yOffset, tt.rock)
+			assert.Equal(t, tt.after, cave.rows)
+			assert.Equal(t, tt.size, cave.topRock())
+		})
+	}
+}
