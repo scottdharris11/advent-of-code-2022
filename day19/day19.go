37caef925f8fa94a65d92e6a4bfb7a0962dff244
scottdharris <scottdharris11@yahoo.com>
2022-12-24 17:37:42 -0600

    day 19 part 1 solution (extremely slow)

---
+++ day19/day19.go
@@ -0,0 +1,213 @@
+package day19
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"time"
+
+	"advent-of-code-2022/utils"
+)
+
+type Puzzle struct{}
+
+func (Puzzle) Solve() {
+	input := utils.ReadLines("day19", "day-19-input.txt")
+	solvePart1(input)
+	solvePart2(input)
+}
+
+func solvePart1(lines []string) int {
+	start := time.Now().UnixMilli()
+	ans := 0
+	for _, line := range lines {
+		ans += NewBlueprint(line).QualityLevel()
+	}
+	end := time.Now().UnixMilli()
+	log.Printf("Day 19, Part 1 (%dms): Quality Level = %d", end-start, ans)
+	return ans
+}
+
+func solvePart2(lines []string) int {
+	start := time.Now().UnixMilli()
+	ans := len(lines)
+	end := time.Now().UnixMilli()
+	log.Printf("Day 19, Part 2 (%dms): Exterior Sides = %d", end-start, ans)
+	return ans
+}
+
+var robotCostParser = regexp.MustCompile(`([\d]+) (ore|clay|obsidian)`)
+
+func NewRobotCost(s string) RobotCost {
+	r := RobotCost{}
+	matches := robotCostParser.FindAllStringSubmatch(s, -1)
+	for _, m := range matches {
+		switch m[2] {
+		case "ore":
+			r.ore = utils.Number(m[1])
+		case "clay":
+			r.clay = utils.Number(m[1])
+		case "obsidian":
+			r.obsidian = utils.Number(m[1])
+		}
+	}
+	return r
+}
+
+type RobotCost struct {
+	ore      int
+	clay     int
+	obsidian int
+}
+
+var blueprintParser = regexp.MustCompile(`Blueprint ([\d]+): ([^.]+). ([^.]+). ([^.]+). ([^.]+).`)
+
+func NewBlueprint(line string) *Blueprint {
+	match := blueprintParser.FindStringSubmatch(line)
+	if match == nil {
+		return nil
+	}
+	return &Blueprint{
+		id:       utils.Number(match[1]),
+		ore:      NewRobotCost(match[2]),
+		clay:     NewRobotCost(match[3]),
+		obsidian: NewRobotCost(match[4]),
+		geode:    NewRobotCost(match[5]),
+	}
+}
+
+type Blueprint struct {
+	id       int
+	ore      RobotCost
+	clay     RobotCost
+	obsidian RobotCost
+	geode    RobotCost
+}
+
+func (b *Blueprint) QualityLevel() int {
+	m := utils.MaxFinder{Maximizer: b}
+	s, max := m.Max(BlueprintState{oreRobot: 1}, nil, 0)
+	fmt.Printf("ID: %d, Max geode: %d, State: %v\n", b.id, max, s)
+	return max * b.id
+}
+
+func (b *Blueprint) Goal(state interface{}) (bool, int) {
+	var bState = state.(BlueprintState)
+	return bState.minute == 24, bState.geode
+}
+
+func (b *Blueprint) PossibleNextStates(state interface{}, currentMax int) []interface{} {
+	var bState = state.(BlueprintState)
+
+	var moves []interface{}
+	base := bState.advanceMinute()
+	if goal, _ := b.Goal(base); goal {
+		moves = append(moves, base)
+		return moves
+	}
+	if !base.canAchieve(currentMax) {
+		return nil
+	}
+
+	buyCnt := 0
+	if bState.canBuy(b.geode) {
+		buyCnt++
+		moves = append(moves, base.buyGeodeRobot(b.geode, &bState))
+	}
+	if base.minute < 23 && bState.canBuy(b.obsidian) {
+		buyCnt++
+		moves = append(moves, base.buyObsidianRobot(b.obsidian, &bState))
+	}
+	if base.minute < 22 && bState.canBuy(b.clay) {
+		buyCnt++
+		moves = append(moves, base.buyClayRobot(b.clay, &bState))
+	}
+	if base.minute < 23 && bState.canBuy(b.ore) {
+		buyCnt++
+		moves = append(moves, base.buyOreRobot(b.ore, &bState))
+	}
+	if buyCnt < 4 {
+		moves = append(moves, base)
+	}
+	return moves
+}
+
+type BlueprintState struct {
+	parent        *BlueprintState
+	minute        int
+	oreRobot      int
+	ore           int
+	clayRobot     int
+	clay          int
+	obsidianRobot int
+	obsidian      int
+	geodeRobot    int
+	geode         int
+}
+
+func (b BlueprintState) advanceMinute() BlueprintState {
+	return BlueprintState{
+		parent:        &b,
+		minute:        b.minute + 1,
+		oreRobot:      b.oreRobot,
+		ore:           b.ore + b.oreRobot,
+		clayRobot:     b.clayRobot,
+		clay:          b.clay + b.clayRobot,
+		obsidianRobot: b.obsidianRobot,
+		obsidian:      b.obsidian + b.obsidianRobot,
+		geodeRobot:    b.geodeRobot,
+		geode:         b.geode + b.geodeRobot,
+	}
+}
+
+func (b BlueprintState) canBuy(cost RobotCost) bool {
+	return b.ore >= cost.ore &&
+		b.clay >= cost.clay &&
+		b.obsidian >= cost.obsidian
+}
+
+func (b BlueprintState) buyOreRobot(cost RobotCost, parent *BlueprintState) BlueprintState {
+	s := b.buy(cost, parent)
+	s.oreRobot++
+	return s
+}
+
+func (b BlueprintState) buyClayRobot(cost RobotCost, parent *BlueprintState) BlueprintState {
+	s := b.buy(cost, parent)
+	s.clayRobot++
+	return s
+}
+
+func (b BlueprintState) buyObsidianRobot(cost RobotCost, parent *BlueprintState) BlueprintState {
+	s := b.buy(cost, parent)
+	s.obsidianRobot++
+	return s
+}
+
+func (b BlueprintState) buyGeodeRobot(cost RobotCost, parent *BlueprintState) BlueprintState {
+	s := b.buy(cost, parent)
+	s.geodeRobot++
+	return s
+}
+
+func (b BlueprintState) buy(cost RobotCost, parent *BlueprintState) BlueprintState {
+	return BlueprintState{
+		parent:        parent,
+		minute:        b.minute,
+		oreRobot:      b.oreRobot,
+		ore:           b.ore - cost.ore,
+		clayRobot:     b.clayRobot,
+		clay:          b.clay - cost.clay,
+		obsidianRobot: b.obsidianRobot,
+		obsidian:      b.obsidian - cost.obsidian,
+		geodeRobot:    b.geodeRobot,
+		geode:         b.geode,
+	}
+}
+
+func (b BlueprintState) canAchieve(val int) bool {
+	minutes := 24 - b.minute + 1
+	future := b.geodeRobot * minutes
+	future += (minutes * (minutes - 1)) / 2
+	return b.geode+future > val
+}
---
+++ day19/day19_test.go
@@ -0,0 +1,58 @@
+package day19
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"advent-of-code-2022/utils"
+)
+
+var testInput = []string{
+	"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
+	"Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
+}
+
+func TestSolvePart1(t *testing.T) {
+	assert.Equal(t, 33, solvePart1(testInput))
+	assert.Equal(t, 1834, solvePart1(utils.ReadLines("day19", "day-19-input.txt")))
+}
+
+func TestSolvePart2(t *testing.T) {
+	assert.Equal(t, 0, solvePart2(testInput))
+	assert.Equal(t, 0, solvePart2(utils.ReadLines("day19", "day-19-input.txt")))
+}
+
+func TestNewBlueprint(t *testing.T) {
+	tests := []struct {
+		line      string
+		blueprint Blueprint
+	}{
+		{
+			"Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.",
+			Blueprint{
+				id:       1,
+				ore:      RobotCost{ore: 4},
+				clay:     RobotCost{ore: 2},
+				obsidian: RobotCost{ore: 3, clay: 14},
+				geode:    RobotCost{ore: 2, obsidian: 7},
+			},
+		},
+		{
+			"Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.",
+			Blueprint{
+				id:       2,
+				ore:      RobotCost{ore: 2},
+				clay:     RobotCost{ore: 3},
+				obsidian: RobotCost{ore: 3, clay: 8},
+				geode:    RobotCost{ore: 3, obsidian: 12},
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.line, func(t *testing.T) {
+			assert.Equal(t, tt.blueprint, *NewBlueprint(tt.line))
+		})
+	}
+}
