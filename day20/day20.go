110dd23b321ad93012d13bc4b12e7ea8da94d203
scottdharris <scottdharris11@yahoo.com>
2025-01-30 09:43:28 -0500

    day 20 part 1

--- day20/day20.go
+++ day20/day20.go
@@ -2,6 +2,7 @@ package day20
 
 import (
 	"log"
+	"math"
 	"time"
 
 	"advent-of-code-2022/utils"
@@ -17,28 +18,14 @@ func (Puzzle) Solve() {
 
 func solvePart1(lines []string) int {
 	start := time.Now().UnixMilli()
-
-	list := utils.WrappedList{}
-	var items []*utils.WrappedListItem
-	for _, line := range lines {
-		items = append(items, list.Add(utils.Number(line)))
+	coords, zero := parseCoordinates(lines)
+	count := len(coords)
+	for _, c := range coords {
+		move(c, count)
 	}
-
-	for _, item := range items {
-		list.Move(item, item.Value.(int))
-	}
-
-	zeroIdx := 0
-	for _, value := range list.Items() {
-		if value.(int) == 0 {
-			break
-		}
-		zeroIdx++
-	}
-
-	var i1000 = list.ItemAtIndex(zeroIdx + 1000).(int)
-	var i2000 = list.ItemAtIndex(zeroIdx + 2000).(int)
-	var i3000 = list.ItemAtIndex(zeroIdx + 3000).(int)
+	i1000 := valueAfter(zero, count, 1000)
+	i2000 := valueAfter(zero, count, 2000)
+	i3000 := valueAfter(zero, count, 3000)
 	ans := i1000 + i2000 + i3000
 	end := time.Now().UnixMilli()
 	log.Printf("Day 20, Part 1 (%dms): Coordinate = %d", end-start, ans)
@@ -51,4 +38,68 @@ func solvePart2(lines []string) int {
 	end := time.Now().UnixMilli()
 	log.Printf("Day 20, Part 2 (%dms): Answer = %d", end-start, ans)
 	return ans
+}
+
+type Coordinate struct {
+	value int
+	next  *Coordinate
+	prev  *Coordinate
+}
+
+func parseCoordinates(lines []string) ([]*Coordinate, *Coordinate) {
+	var coords []*Coordinate
+	var zero *Coordinate
+
+	first := &Coordinate{value: utils.Number(lines[0])}
+	coords = append(coords, first)
+	prev := first
+	for i := 1; i < len(lines); i++ {
+		next := &Coordinate{value: utils.Number(lines[i])}
+		if next.value == 0 {
+			zero = next
+		}
+		coords = append(coords, next)
+		next.prev = prev
+		prev.next = next
+		prev = next
+	}
+	first.prev = prev
+	prev.next = first
+	return coords, zero
+}
+
+func move(c *Coordinate, count int) {
+	places := int(math.Abs(float64(c.value)))
+	prev := c.value < 0
+	if prev {
+		places++
+	}
+	places %= count - 1
+	if places == 0 {
+		return
+	}
+	current := c
+	for i := 0; i < places; i++ {
+		if prev {
+			current = current.prev
+		} else {
+			current = current.next
+		}
+	}
+
+	c.prev.next = c.next
+	c.next.prev = c.prev
+	c.prev = current
+	c.next = current.next
+	c.next.prev = c
+	c.prev.next = c
+}
+
+func valueAfter(c *Coordinate, count int, places int) int {
+	current := c
+	adjust := places % count
+	for i := 0; i < adjust; i++ {
+		current = current.next
+	}
+	return current.value
 }
--- day20/day20_test.go
+++ day20/day20_test.go
@@ -12,7 +12,7 @@ var testInput = []string{"1", "2", "-3", "3", "-2", "0", "4"}
 
 func TestSolvePart1(t *testing.T) {
 	assert.Equal(t, 3, solvePart1(testInput))
-	assert.Equal(t, 0, solvePart1(utils.ReadLines("day20", "day-20-input.txt")))
+	assert.Equal(t, 7228, solvePart1(utils.ReadLines("day20", "day-20-input.txt")))
 }
 
 func TestSolvePart2(t *testing.T) {
